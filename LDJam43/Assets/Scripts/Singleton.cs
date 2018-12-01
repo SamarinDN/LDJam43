670ada5f0fbaad0d35ca7995334f41c4b3965a7b
alexey.tarabrin <alexey.tarabrin@matryoshkagames.com>
2018-12-01 20:12:30 +0700

    Chin + Char

--- LDJam43/Assets/Scripts/Singleton.cs
+++ LDJam43/Assets/Scripts/Singleton.cs
@@ -2,23 +2,21 @@
 
 public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 {
-	protected static T instance;
+	static T _instance;
 
 	public static T Instance
 	{
 		get
 		{
-			if (instance == null)
-			{
-				instance = (T) FindObjectOfType(typeof(T));
+			if ( _instance != null ) return _instance;
+			_instance = (T) FindObjectOfType(typeof(T));
 
-				if (instance == null)
-				{
-					Debug.LogError("An instance of " + typeof(T) + " is needed in the scene, but there is none.");
-				}
+			if (_instance == null)
+			{
+				Debug.LogError("An instance of " + typeof(T) + " is needed in the scene, but there is none.");
 			}
 
-			return instance;
+			return _instance;
 		}
 	}
 }
