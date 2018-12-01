50f5f79ab4cdf5c7f74d788e2945dcafb1dfad98
Samarin Daniil <danil.samarin@matryoshkagames.com>
2018-12-01 16:12:51 +0700

    prototype parallax + DOTween

---
+++ LDJam43/Assets/Scripts/Parallax.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Parallax : MonoBehaviour {
+
+	[SerializeField] private Transform _image;
+	private float _pos = 0;
+	public float Delta = 0;
+	public float Speed = 0;
+
+	void FixedUpdate() {
+		// TODO
+		_pos += Speed;
+		if (_pos > Delta) {
+			_pos -= Delta;
+		}
+		_image.position = new Vector3(-_pos, _image.position.y);
+	}
+}
