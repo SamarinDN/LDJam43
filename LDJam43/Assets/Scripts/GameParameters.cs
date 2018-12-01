c4c180698e40e5fe005be52aae3b129a7f7e6513
alexey.tarabrin <alexey.tarabrin@matryoshkagames.com>
2018-12-01 14:25:46 +0700

    GodClass Start

---
+++ LDJam43/Assets/Scripts/GameParameters.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Lifetime;
+using UnityEngine;
+
+public class GameParameters : Singleton<GameParameters> {
+	public float FuelInUsage = 70;
+	public float FuelInStorage;
+	public int   Lives       = 5;
+	public float TimePerTick = 1;
+
+	public float Speed {
+		get { return FuelInStorage; }
+	}
+
+	void Start() {
+		StartCoroutine(Tick());
+	}
+
+	IEnumerator Tick() {
+		TickAction();
+		yield return new WaitForSeconds(TimePerTick);
+		StartCoroutine(Tick());
+	}
+
+	void TickAction() {
+		FuelInUsage -= 1;
+	}
+
+	public void DealDamage(int lives) {
+		Lives -= lives;
+	}
+	
+	
+}
