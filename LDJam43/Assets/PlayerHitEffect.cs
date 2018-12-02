88c972d0a521726de6a1cf86d2434b132648ce19
alexey.tarabrin <alexey.tarabrin@matryoshkagames.com>
2018-12-02 12:24:05 +0700

    Генератор

--- LDJam43/Assets/PlayerHitEffect.cs
+++ LDJam43/Assets/PlayerHitEffect.cs
@@ -5,21 +5,29 @@ using UnityEngine;
 public class PlayerHitEffect : MonoBehaviour {
 	BoxCollider2D _collider2D;
 
+		Coroutine coroutine;
 	void Awake() {
 		_collider2D = GetComponent<BoxCollider2D>();
 	}
 
-	IEnumerator CheckCollide() {
+	void OnEnable() {
+		coroutine = StartCoroutine(CheckCollider());
+	}
+
+	void OnDisable() {
+		if ( coroutine != null ) StopCoroutine(coroutine);
+	}
+
+	IEnumerator CheckCollider() {
 		while ( true ) {
-			
-		
-		yield return new WaitForSeconds(0.2f);
-		foreach ( var other in Physics2D.OverlapBoxAll((Vector2) transform.position + _collider2D.offset,
-			_collider2D.size, 0f) ) {
-			if ( !other.CompareTag("Player") ) continue;
-			EventManager.StoleSoul();
-		//	_image.sprite = _beforePlayerHit;
-			yield break;
-		}}
+			yield return new WaitForSeconds(0.2f);
+			foreach ( var other in Physics2D.OverlapBoxAll((Vector2) transform.position + _collider2D.offset,
+				_collider2D.size, 0f) ) {
+				if ( !other.CompareTag("Player") ) continue;
+				EventManager.StoleSoul();
+				//	_image.sprite = _beforePlayerHit;
+				yield break;
+			}
+		}
 	}
 }
